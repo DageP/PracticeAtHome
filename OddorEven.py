0be11d8c0c08fdb0a789f97ccbc1be293524cc83
DageP <58255072+DageP@users.noreply.github.com>
2019-11-30 20:49:46 +0300

    Add files via upload

---
+++ OddorEven.py
@@ -0,0 +1,6 @@
+number = int(input("Please enter a number : "))
+decision = number % 2
+if decision == 0:
+    print("The number %d is even " %(number))
+else:
+    print("The number %d is odd" %(number))
