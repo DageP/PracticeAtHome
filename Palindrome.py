df38fa27454436aefdc330c1333df86f83aa713f
DageP <58255072+DageP@users.noreply.github.com>
2019-12-02 20:22:47 +0300

    Add files via upload

---
+++ Palindrome.py
@@ -0,0 +1,7 @@
+Palindrome = str(input("PLease enter a word : "))
+reverse = (Palindrome[::-1])
+if reverse == Palindrome:
+    print("The word is a palindrome")
+else:
+    print("The word is not a palindrome")
+    
