ddf02bda2444cd0e27a20ac8c54ab0caf3524654
DageP <58255072+DageP@users.noreply.github.com>
2019-12-08 18:14:25 +0300

    Add files via upload
    List Overlap Comprehensions

--- ListOverlap.py
+++ ListOverlap.py
@@ -1,13 +1,11 @@
-a = []
-b = []
 c = []
 import random
-number = range(1, random.randint(1,150))
-number1 = range(1, random.randint(1,500))
-for x in number:
-    a.append(x)
-for x in number1:
-    b.append(x)
+number = random.sample(range(1, random.randrange(150)), random.randint(1,10))
+number1 = random.sample(range(1, random.randrange(500)), random.randint(1,10))
+a = [x for x in number]
+b = [x for x in number1]
+print(a)
+print(b)
 for x in a:
     if x == x in b:
         if x not in c:
