c0ae715e92c668b788df0496647ad6c5ab124671
DageP <58255072+DageP@users.noreply.github.com>
2020-05-13 12:50:42 +0300

    Create RemovingDuplicatesInDictionary.py

---
+++ RemovingDuplicatesInDictionary.py
@@ -0,0 +1,27 @@
+student_data = {'id1': 
+   {'name': ['Sara'], 
+    'class': ['V'], 
+    'subject_integration': ['english, math, science']
+   },
+ 'id2': 
+  {'name': ['David'], 
+    'class': ['V'], 
+    'subject_integration': ['english, math, science']
+   },
+ 'id3': 
+    {'name': ['Sara'], 
+    'class': ['V'], 
+    'subject_integration': ['english, math, science']
+   },
+ 'id4': 
+   {'name': ['Surya'], 
+    'class': ['V'], 
+    'subject_integration': ['english, math, science']
+   },
+}
+
+Removed = {}
+for x,y in student_data.items(): #x is the key whilst y is the value
+    if y not in Removed.values(): #"if value not in Removed.values()"
+        Removed[x] = y #adds new keys and values into removed
+print(Removed)
